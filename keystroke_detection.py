090f220b4ede4478e956afbedb46bcd15d8e9f12
anhtu-pham <anhtupham99999@gmail.com>
2025-02-23 00:19:13 -0500

    Add information from keystroke detection to file

--- keystroke_detection.py
+++ keystroke_detection.py
@@ -1,7 +1,10 @@
 from pynput import keyboard
 import time
+import csv
 
+fieldnames = ["Key", "Timestamp"]
 key_events = []
+log_file = 'real.csv'
 
 def on_press(key):
     keyValue = None
@@ -9,12 +12,18 @@ def on_press(key):
         keyValue = key.char
     except AttributeError:
         keyValue = str(key)
-    timestamp = time.time()
-    print(f"\nKey: {keyValue} Timestamp: {timestamp}")
-    key_events.append({"Key": keyValue, "Timestamp": timestamp})
+    key_event = {fieldnames[0]: str(key), fieldnames[1]: time.time()}
+    print(f'\n{key_event}')
+    key_events.append(key_event)
 
 def on_release(key):
     if key == keyboard.Key.esc:
+        with open(log_file, mode='a', newline='') as file:
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+            if file.tell() == 0:
+                writer.writeheader()
+            for key_event in key_events:
+                writer.writerow(key_event)
         return False
 
 with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:

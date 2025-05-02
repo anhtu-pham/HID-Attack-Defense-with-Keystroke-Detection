41d8f46041c3e8d6979ef6e8ea981a0643d2515e
ShaunNunoo <san71@case.edu>
2025-05-02 17:02:08 -0400

    Template UI

---
+++ hid_attacker_defence_app/android/app/src/main/kotlin/com/example/hid_attacker_defence_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.hid_attacker_defence_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ hid_defence_ui/android/app/src/main/kotlin/com/example/hid_defence_ui/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.hid_defence_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
